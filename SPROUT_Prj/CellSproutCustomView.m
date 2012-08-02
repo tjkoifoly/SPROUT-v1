a90f16b9a292fc5ed35464f3133cc39cf4d4e869
Nguyen Chi Cong <foly.v1.8.01@gmail.com>
2012-08-02 11:28:40 +0700

    update

---
+++ SPROUT_Prj/CellSproutCustomView.m
@@ -0,0 +1,29 @@
+//
+//  CellSproutCustomView.m
+//  SPROUT_Prj
+//
+//  Created by Nguyen Chi Cong on 8/2/12.
+//  Copyright (c) 2012 BKHN. All rights reserved.
+//
+
+#import "CellSproutCustomView.h"
+
+@implementation CellSproutCustomView
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
