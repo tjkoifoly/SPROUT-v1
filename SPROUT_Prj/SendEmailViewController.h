a0ee27f9441a47a51ca61d6c7784092d4915d560
Nguyen Chi Cong <foly.v1.8.01@gmail.com>
2012-08-03 15:28:24 +0700

    UPDATE protorype

---
+++ SPROUT_Prj/SendEmailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendEmailViewController.h
+//  SPROUT_Prj
+//
+//  Created by Nguyen Chi Cong on 8/3/12.
+//  Copyright (c) 2012 BKHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendEmailViewController : UIViewController
+
+@end
---
+++ SPROUT_Prj/ViewPhotoInSproutViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewPhotoInSproutViewController.h
+//  SPROUT_Prj
+//
+//  Created by Nguyen Chi Cong on 8/3/12.
+//  Copyright (c) 2012 BKHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewPhotoInSproutViewController : UIViewController
+
+@end
